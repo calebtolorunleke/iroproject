b5f39d2ded9b5e29a82127cf4905c915ed64c878
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-08-03 17:55:34 -0700

    update on iro project

--- src/components/DataFetching.jsx
+++ src/components/DataFetching.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
+import { PropagateLoader } from "react-spinners";
 
 const url = "https://jsonplaceholder.typicode.com/users";
 
 const DataFetching = () => {
   const [users, setUser] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getUsers = async () => {
@@ -13,6 +15,7 @@ const DataFetching = () => {
         const data = await response.json();
         console.log(data);
         setUser(data);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -21,6 +24,15 @@ const DataFetching = () => {
 
     //fetch req url using js fetch promises .then and async wait using asynch code... return future value to us
   }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col justify-center items-center h-screen">
+        {/* <h1 className="text-center text-6xl font-bold">Loading....</h1> */}
+        <PropagateLoader size={50} />
+      </div>
+    );
+  }
 
   return (
     <div>
